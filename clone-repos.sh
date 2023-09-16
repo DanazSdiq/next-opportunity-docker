9e58072db16fd082698a05ecc9641c0e9476a000
danazsdiq <danazsdiq17@gmail.com>
2023-09-16 18:52:05 +0300

    init repo

---
+++ clone-repos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/DanazSdiq/next-opportunity-client
+git clone https://github.com/DanazSdiq/next-opportunity-server
+git clone https://github.com/DanazSdiq/next-opportunity-generation-pipeline
+git clone https://github.com/DanazSdiq/next-opportunity-migrations
